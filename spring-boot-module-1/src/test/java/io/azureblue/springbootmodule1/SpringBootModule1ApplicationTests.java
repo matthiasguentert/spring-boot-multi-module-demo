c733234b5a0da6fd1108c675b2a092e21345adf7
Matthias Guentert <matthias.guentert@solveva.com>
2022-04-01 20:54:48 +0200

    Initial commit

---
+++ spring-boot-module-1/src/test/java/io/azureblue/springbootmodule1/SpringBootModule1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.azureblue.springbootmodule1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootModule1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-boot-module-2/src/test/java/io/azureblue/springbootmodule2/SpringBootModule2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.azureblue.springbootmodule2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootModule2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
